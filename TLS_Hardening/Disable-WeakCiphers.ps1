ee0e0b980d6a9f0632af0c468887cf74f152b3f7
VB-Cloudboy <vijaykborkar@outlook.com>
2019-04-30 11:34:59 +0530

    Security Scripts for Hardening and TLS checks

---
+++ TLS_Hardening/Disable-WeakCiphers.ps1
@@ -0,0 +1,43 @@
+﻿<#
+.Synopsis
+   Script to check and disable Ciphers
+
+.DESCRIPTION
+   Disabling below Cipher Suite
+    "TLS_RSA_WITH_RC4_128_SHA"
+    "TLS_RSA_WITH_RC4_128_MD5"
+    "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+    "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256"
+    "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384"
+
+.EXAMPLE
+   
+   DisableCipher -Cipher "TLS_RSA_WITH_RC4_128_SHA"
+
+.NOTES
+ Name : Disable-WeakCiphers.ps1
+ Author : Vijay Borkar
+ Version : 0.1
+#>
+
+
+
+function DisableWeak-Cipher($Cipher)
+{
+if(Get-TlsCipherSuite -Name $Cipher)
+{
+    Disable-TlsCipherSuite -Name $Cipher
+    Write-Host "$Cipher - Disabled" -f Green
+}
+else
+{
+    Write-Host "$cipher was already in disabled state" -f Cyan
+}
+}
+
+DisableWeak-Cipher "TLS_RSA_WITH_RC4_128_SHA"
+DisableWeak-Cipher "TLS_RSA_WITH_RC4_128_MD5"
+DisableWeak-Cipher "TLS_RSA_WITH_3DES_EDE_CBC_SHA"
+DisableWeak-Cipher "TLS_DHE_RSA_WITH_AES_128_GCM_SHA256"
+DisableWeak-Cipher "TLS_DHE_RSA_WITH_AES_256_GCM_SHA384"
+
---
+++ TLS_Hardening/TLS-FIPS-Compliant-v1.1.ps1
@@ -0,0 +1,55 @@
+﻿<#
+.Synopsis
+   PS-Script to perform the TLS-FIPS compliance 
+
+.DESCRIPTION
+   The following script check the compliance with the security standards like "TLS and FIPS". 
+
+
+.NOTES
+ Name : TLS-FIPS-Compliant.ps1
+ Author : Vijay Borkar
+ Version : 1.1
+#>
+
+#1.Initialization of Directory & File
+Function Initialize-Directory {
+
+$Folder = "TLS-FIPS"
+$FolderPath = "$($env:USERPROFILE)\Desktop\TLS-FIPS"
+$File = "TLSFIPS.csv"
+$FilePath = $FolderPath
+$Csvheaders = @("Name", "IP-Address")
+$TempFolder = Test-Path -Path "$($env:USERPROFILE)\Desktop\TLS-FIPS"
+    If(!($TempFolder))
+        {
+          New-Item -Path "$($env:USERPROFILE)\Desktop" -Name "$Folder" -ItemType Directory
+          $Csvheaders| select 'Name', 'IP-Address'| Export-Csv -Path "$($FilePath)\$File" -NoTypeInformation
+          Write-Host "Folder named $Folder created on Desktop Successfully" -ForegroundColor Green
+          Write-Host "Please Copy-Paste your file in folder$Folder created on Desktop" -ForegroundColor Green
+         }Else
+              {
+                Write-Host "Please Copy-Paste all Server Names in $File in the folder $Folder created on Desktop" -ForegroundColor Green
+              }
+
+} 
+
+
+#2.Retrive list of server names on which the activity is to be performed
+$List = Import-Csv -LiteralPath "$($FilePath)\$File"
+$ServerList = ($List).name
+
+
+#3.
+
+$password = read-host -prompt "Enter your Password" 
+$secure = ConvertTo-SecureString $password -force -asPlainText 
+$bytes = ConvertFrom-SecureString $secure 
+$bytes | out-file "$($FilePath)\SecPass.txt" #>
+
+
+
+############Getting the credentials Do it only if needed ############
+$EncryptedPW = Get-Content -Path "$($FilePath)\SecPass.txt" 
+$SecureString = ConvertTo-SecureString -String $EncryptedPW 
+$Credentials = New-Object System.Management.Automation.PSCredential "MyUsername", $SecureString
